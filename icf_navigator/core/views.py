318e1e923f78bce43455e66cb6f75790e318dc6f
Joseph Utecht <joseph.utecht@gmail.com>
2020-09-22 13:34:31 -0500

    moved templates

--- icf_navigator/core/views.py
+++ icf_navigator/core/views.py
@@ -5,5 +5,5 @@ from django.contrib.auth import get_user_model
 # Create your views here.
 def home_view(request):
     return render(request,
-                  'navigator/home.html',
+                  'core/home.html',
                   {})
