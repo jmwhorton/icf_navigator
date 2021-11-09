dda26c0ea90d6cdc2487a3f272a71cb321014c48
Joseph Utecht <thenmal@gmail.com>
2021-11-09 13:38:10 -0600

    fixed new backup script

--- icf_navigator/fullbackup.sh
+++ icf_navigator/fullbackup.sh
@@ -1,1 +1,1 @@
-oc exec deployment/navigator -c navigator -- ./manage.py dumpdata > prod_data.json
+oc exec deployment/navigator -c navigator -- ./manage.py dumpdata --exclude auth.permission --exclude contenttypes > prod_data.json
