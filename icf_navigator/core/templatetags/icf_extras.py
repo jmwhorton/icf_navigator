8108bc2b8ba36b1a33c7b8600f7c98a2cdd2298e
Joseph Utecht <jrutecht@uams.edu>
2022-02-07 08:36:43 -0600

    finished forms

--- icf_navigator/core/templatetags/icf_extras.py
+++ icf_navigator/core/templatetags/icf_extras.py
@@ -1,6 +1,7 @@
 from django import template
 from django.template.defaultfilters import stringfilter
-from django.utils.html import format_html
+from django.utils.html import format_html, format_html_join
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -21,8 +22,44 @@ def list_value(value, pd):
     return ''
 
 @register.simple_tag()
+def list_values(value, pd):
+    if value not in pd:
+        return ''
+    values = []
+    for x in pd[value]:
+        if x:
+            values.append([x])
+    return format_html_join('\n', '<li>{}</li>', values)
+
+@register.simple_tag()
 def percent(a, b):
     if(a and b):
         return (int(a)/int(b)) * 100
     else:
         return 0
+
+@register.simple_tag()
+def in_list(value, key, pd):
+    return value in pd[key]
+
+@register.simple_tag()
+def explain_text(value, pd, et):
+    if pd.get(value) == True:
+        return mark_safe(et.get(value))
+    elif pd.get(value):
+        return mark_safe(pd[value])
+    else:
+        return ''
+
+@register.simple_tag()
+def explain_text_li(value, pd, et):
+    if pd.get(value) == True:
+        return format_html('<li>{}</li>', et.get(value))
+    elif pd.get(value):
+        return format_html('<li>{}</li>', pd.get(value))
+    else:
+        return ''
+
+@register.simple_tag()
+def is_true(key, pd):
+    return pd.get(key) == True
