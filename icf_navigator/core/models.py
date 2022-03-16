144d401ebbeacc02438531164253f236f3aee02a
Joseph Utecht <jrutecht@uams.edu>
2022-03-16 10:00:56 -0500

    readability score framework

--- icf_navigator/core/models.py
+++ icf_navigator/core/models.py
@@ -4,6 +4,7 @@ from users.models import ADUser, PotentialUser
 from typedmodels.models import TypedModel
 import datetime
 
+
 class ConsentForm(models.Model):
     study_name = models.CharField(max_length=500)
     authorized_users = models.ManyToManyField(PotentialUser)
@@ -49,7 +50,6 @@ class ConsentForm(models.Model):
         return self.study_name[:20]
 
 
-
 class CannedText(models.Model):
     label = models.CharField(max_length=50, unique=True)
     text = models.TextField(blank=True)
@@ -57,19 +57,33 @@ class CannedText(models.Model):
     def __str__(self):
         return self.label
 
+
 class Question(TypedModel):
     text = models.TextField(blank=True)
     extra_text = models.TextField(blank=True)
     order = models.FloatField(unique=True)
     label = models.CharField(max_length=50, unique=True)
-    canned_yes = models.ForeignKey(CannedText, on_delete=models.CASCADE, blank=True, null=True, related_name="canned_yes")
-    canned_no = models.ForeignKey(CannedText, on_delete=models.CASCADE, blank=True, null=True, related_name="canned_no")
+    canned_yes = models.ForeignKey(
+        CannedText,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="canned_yes",
+    )
+    canned_no = models.ForeignKey(
+        CannedText,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="canned_no",
+    )
 
     def __str__(self):
         return self.label
 
     class Meta:
-        ordering = ['order']
+        ordering = ["order"]
+
 
 class Response(models.Model):
     form = models.ForeignKey(ConsentForm, on_delete=models.CASCADE)
@@ -77,19 +91,22 @@ class Response(models.Model):
     data = models.JSONField(blank=True, null=True)
     last_change = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(ADUser, on_delete=models.CASCADE)
+
     class Meta:
-        unique_together = [['form', 'question']]
-        get_latest_by = 'last_change'
+        unique_together = [["form", "question"]]
+        get_latest_by = "last_change"
+
     def __str__(self):
         return "{}[{}]".format(self.form, self.question)
 
     @property
     def is_yes(self):
-        return self.data.get('yes') == True
+        return self.data.get("yes") == True
 
     @property
     def is_no(self):
-        return self.data.get('yes') == False
+        return self.data.get("yes") == False
+
 
 class EditText(models.Model):
     response = models.ForeignKey(Response, on_delete=models.CASCADE)
@@ -97,6 +114,7 @@ class EditText(models.Model):
 
     def __str__(self):
         return "et [{}]".format(self.response)
+
 
 class Section(models.Model):
     name = models.TextField()
@@ -107,115 +125,163 @@ class Section(models.Model):
         return self.name
 
     class Meta:
-        ordering = ['order']
+        ordering = ["order"]
+
 
 class QGroup(models.Model):
     name = models.TextField(blank=True)
     order = models.FloatField()
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
     questions = models.ManyToManyField(Question, blank=True)
-    logic = models.TextField(default='True')
+    logic = models.TextField(default="True")
 
     def __str__(self):
         return self.name
 
     def enabled(self, pd):
         try:
-            ret = eval(self.logic, {'pd': pd})
+            ret = eval(self.logic, {"pd": pd})
             return ret
         except:
             return False
 
     class Meta:
-        ordering = ['order']
-        unique_together = ['order', 'section']
+        ordering = ["order"]
+        unique_together = ["order", "section"]
+
 
 class YesNoForm(forms.Form):
-    yes = forms.TypedChoiceField(label='',
-                             required=True,
-                             coerce=lambda x: x == 'True',
-                             choices=[(True, 'Yes'),(False, 'No')],
-                             widget=forms.RadioSelect)
+    yes = forms.TypedChoiceField(
+        label="",
+        required=True,
+        coerce=lambda x: x == "True",
+        choices=[(True, "Yes"), (False, "No")],
+        widget=forms.RadioSelect,
+    )
+
 
 class YesNoQuestion(Question):
     def form(self, *args, **kwargs):
         return YesNoForm(*args, auto_id=False, **kwargs)
+
     def for_dict(self, data):
-        return data['yes']
+        return data["yes"]
+
 
 class YesNoExplainForm(forms.Form):
     auto_id = False
-    yes = forms.TypedChoiceField(label='',
-                             required=True,
-                             coerce=lambda x: x == 'True',
-                             choices=[(True, 'Yes'),(False, 'No')],
-                             widget=forms.RadioSelect(attrs={'data-yesno-target': 'yesno',
-                                                             'data-action': 'input->yesno#toggled'}))
-    hidden = {'hidden': None, 'data-yesno-target': 'explain', 'placeholder': 'Explain...'}
-    explanation = forms.CharField(label='', required=False, widget=forms.Textarea(attrs=hidden), help_text='Please Explain')
+    yes = forms.TypedChoiceField(
+        label="",
+        required=True,
+        coerce=lambda x: x == "True",
+        choices=[(True, "Yes"), (False, "No")],
+        widget=forms.RadioSelect(
+            attrs={"data-yesno-target": "yesno", "data-action": "input->yesno#toggled"}
+        ),
+    )
+    hidden = {
+        "hidden": None,
+        "data-yesno-target": "explain",
+        "placeholder": "Explain...",
+        "data-readability-target": "text",
+        "data-action": "readability#change",
+    }
+    explanation = forms.CharField(
+        label="",
+        required=False,
+        widget=forms.Textarea(attrs=hidden),
+        help_text="Please Explain",
+    )
+
     def __init__(self, help_text, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['explanation'].help_text = help_text
-
+        self.fields["explanation"].help_text = help_text
 
 
 class YesNoExplainQuestion(Question):
-    YNB_CHOICES = [
-        ('Y', 'Yes'),
-        ('N', 'No'),
-        ('B', 'Both')
-    ]
-    explain_when = models.CharField(blank=True, null=True, max_length=1, choices=YNB_CHOICES)
+    YNB_CHOICES = [("Y", "Yes"), ("N", "No"), ("B", "Both")]
+    explain_when = models.CharField(
+        blank=True, null=True, max_length=1, choices=YNB_CHOICES
+    )
+
     def form(self, *args, **kwargs):
         return YesNoExplainForm(self.extra_text, *args, auto_id=False, **kwargs)
+
     def for_dict(self, data):
-        if self.explain_when == 'B' or (self.explain_when == 'Y' and data['yes']) or (self.explain_when =='N' and data['yes'] == False):
-            return data['explanation']
-        return data['yes']
+        if (
+            self.explain_when == "B"
+            or (self.explain_when == "Y" and data["yes"])
+            or (self.explain_when == "N" and data["yes"] == False)
+        ):
+            return data["explanation"]
+        return data["yes"]
+
 
 class FreeTextForm(forms.Form):
-    text = forms.CharField(label='', required=True, widget=forms.Textarea(attrs={'class': 'form-control'}))
+    text = forms.CharField(
+        label="",
+        required=True,
+        widget=forms.Textarea(
+            attrs={
+                "class": "form-control",
+                "data-readability-target": "text",
+                "data-action": "readability#change",
+            }
+        ),
+    )
+
 
 class FreeTextQuestion(Question):
     def form(self, *args, **kwargs):
         return FreeTextForm(*args, **kwargs)
+
     def for_dict(self, data):
-        return data['text']
+        return data["text"]
+
 
 class MultiSelectForm(forms.Form):
-    options = forms.MultipleChoiceField(label='', widget=forms.CheckboxSelectMultiple)
+    options = forms.MultipleChoiceField(label="", widget=forms.CheckboxSelectMultiple)
+
     def __init__(self, choices, *args, **kwargs):
         super().__init__(*args, **kwargs)
         c = zip(range(len(choices)), choices)
-        self.fields['options'].choices = c
+        self.fields["options"].choices = c
         self.empty_permitted = True
+
 
 class MultiSelectQuestion(Question):
     options = models.JSONField(null=True, blank=True)
+
     def form(self, *args, **kwargs):
         return MultiSelectForm(self.options, *args, **kwargs)
 
     def for_dict(self, data):
         ret = []
-        if('options' not in data.keys()):
+        if "options" not in data.keys():
             return ret
-        for i in data['options']:
+        for i in data["options"]:
             ret.append(self.options[int(i)])
         return ret
+
 
 class TextListingForm(forms.Form):
     def __init__(self, num_required, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for i in range(9):
             required = (i < num_required) or i == 0
-            attrs = {'class': 'form-control',
-                     'data-multitext-target': 'text'}
-            if(not required):
-                attrs['hidden'] = None
-            f = forms.CharField(label='',
-                                required=required,
-                                widget=forms.TextInput(attrs=attrs))
-            self.fields['text_{}'.format(i)] = f
+            attrs = {
+                "class": "form-control",
+                "data-multitext-target": "text",
+                "data-readability-target": "text",
+                "data-action": "readability#change",
+            }
+            if not required:
+                attrs["hidden"] = None
+            f = forms.CharField(
+                label="", required=required, widget=forms.TextInput(attrs=attrs)
+            )
+            self.fields["text_{}".format(i)] = f
+
 
 class TextListQuestion(Question):
     minimum_required = models.IntegerField(null=True, blank=True)
@@ -230,32 +296,50 @@ class TextListQuestion(Question):
             ret.append(data[key])
         return ret
 
+
 class ContactForm(forms.Form):
-    title = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    phone_number = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    address = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
+    title = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    name = forms.CharField(
+        required=True, widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    email = forms.EmailField(
+        required=True, widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    phone_number = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    address = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control"}))
+
 
 class ContactQuestion(Question):
     def form(self, *args, **kwargs):
         return ContactForm(*args, **kwargs)
+
     def for_dict(self, data):
         return data
 
+
 class IntegerForm(forms.Form):
-    number = forms.IntegerField(label="", required=True, widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    number = forms.IntegerField(
+        label="",
+        required=True,
+        widget=forms.NumberInput(attrs={"class": "form-control"}),
+    )
+
 
 class IntegerQuestion(Question):
     def form(self, *args, **kwargs):
         return IntegerForm(*args, **kwargs)
+
     def for_dict(self, data):
-        return data['number']
+        return data["number"]
+
 
 class CustomQuestion(Question):
     custom_form = models.CharField(blank=True, null=True, max_length=50)
 
     def form(self, *args, **kwargs):
         pass
+
     def for_dict(self, data):
         return data
